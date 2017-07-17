c01b932cc66316e27a68ae650781a695bbe39e91
HenryDane <aglareb7@gmail.com>
2017-07-16 17:26:12 -0700

    beginning scripts, minor fixes

---
+++ doVolunteerAndReturn.php
@@ -0,0 +1,11 @@
+<?php
+	// connect to database (  host       user    password      db 
+	$conn = mysqli_connect("localhost", "blog", "d4b411d4y", "eunioa");
+	if (!$conn) {
+		die("Connection failed: " . mysqli_connect_error());
+		exit();
+	}
+
+	// catch data
+	
+?>
