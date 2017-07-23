26bdd5f0211a6bcfc599cd389d56a62ed9c5473d
HenryDane <aglareb7@gmail.com>
2017-07-23 13:54:47 -0700

    added 404 resources, script work

--- doGenericAndReturn.php
+++ doGenericAndReturn.php
@@ -5,4 +5,19 @@
 		die("Connection failed: " . mysqli_connect_error());
 		exit();
 	}
+	
+	// catch data 
+	$name = mysqli_real_escape_string($conn, $_GET['name']);
+	$email = mysqli_real_escape_string($conn, $_GET['email']);
+	$subject = mysqli_real_escape_string($conn, $_GET['subject']);
+	$message = mysqli_real_escape_string($conn, $_GET['message']);
+	
+	// insert row
+	$sql = "INSERT INTO volunteerMsgs (name, subject, email, message) VALUES ($name, $subject, $email, $message)";
+	if (!mysqli_query($con,$sql)) {
+		die('Error: ' . mysqli_error($con));
+	}
+	echo "1 record added";
+	
+	mysqli_close($conn);
 ?>
